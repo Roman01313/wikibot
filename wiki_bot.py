43d8c6f5bd6562060210cc0bd0dcdb461c523595
Roman <129669519+Roman01313@users.noreply.github.com>
2024-12-21 14:17:44 +0000

    ver 2

--- wiki_bot.py
+++ wiki_bot.py
@@ -8,17 +8,17 @@ country = input("country name...")
 
 wikipedia.set_lang('en')
 page = wikipedia.page(f"Cities of {country}")
-
+# print(wikipedia.page('Cities of England').url)
 
 req = requests.get(page.url)
 soup = BeautifulSoup(req.text, 'lxml')
 table = soup.find_all('table')
 
 first_column_values = []
-rows = table[0].find_all('tr')  # Находим все строки таблицы
+rows = table[0].find_all('tr') 
 for row in rows:
-    first_cell = row.find('td')  # Находим первую ячейку строки
-    if first_cell:  # Проверяем, что ячейка существует
-        first_column_values.append(first_cell.get_text(strip=True))
+    first_cell = row.find('td')
+    if first_cell:
+        first_column_values.append(first_cell.get_text())
 
 print(first_column_values)
